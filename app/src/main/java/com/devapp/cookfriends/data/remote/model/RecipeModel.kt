386e6c6d7dc05bc93a805f98998383b0dbdaa771
francisco <franciscoarenas@outlook.com>
2025-06-02 20:50:33 -0300

    change ids to uuids in remote models

--- app/src/main/java/com/devapp/cookfriends/data/remote/model/RecipeModel.kt
+++ app/src/main/java/com/devapp/cookfriends/data/remote/model/RecipeModel.kt
@@ -2,15 +2,17 @@ package com.devapp.cookfriends.data.remote.model
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Serializable
-data class RecipeModel(
-    @SerialName("id"          ) var id          : Int?                   = null,
+data class RecipeModel @OptIn(ExperimentalUuidApi::class) constructor(
+    @SerialName("id"          ) var id          : Uuid,
     @SerialName("name"        ) var name        : String?                = null,
     @SerialName("description"        ) var description        : String?                = null,
     @SerialName("portions"    ) var portions    : Int?                   = null,
-    @SerialName("userId"    ) var userId    : Int?                   = null,
-    @SerialName("TypeId"    ) var typeId    : Int?                   = null,
+    @SerialName("userId"    ) var userId    : Uuid,
+    @SerialName("typeId"    ) var typeId    : Uuid,
     @SerialName("author"      ) var author      : String?                = null,
     @SerialName("type"        ) var type        : String?                = null,
     //@SerialName("rate"        ) var rate        : Double?                = null,
--- app/src/main/java/com/devapp/cookfriends/data/remote/model/StepModel.kt
+++ app/src/main/java/com/devapp/cookfriends/data/remote/model/StepModel.kt
@@ -2,11 +2,13 @@ package com.devapp.cookfriends.data.remote.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Serializable
-data class StepModel (
-    @SerialName("id"      ) var id        : Int?    = null,
+data class StepModel @OptIn(ExperimentalUuidApi::class) constructor(
+    @SerialName("id"      ) var id        : Uuid,
     @SerialName("order"   ) var order     : Int?    = null,
     @SerialName("content" ) var content   : String? = null,
-    @SerialName("recipeId") var recipeId  : Int?    = null
+    @SerialName("recipeId") var recipeId  : Uuid
 )
